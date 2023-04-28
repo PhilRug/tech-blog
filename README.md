73753aee45e2be5dbd1c4c0218bd189171f3d85e
PhilRug <ruggiero.pj@gmail.com>
2023-04-28 19:40:31 -0400

    finished all models

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # tech-blog
 a blog that members can post messages about technology tips
+
+
+- boiler plate code copied from 14-MVC mini project
--- models/Comment.js
+++ models/Comment.js
@@ -1,6 +1,6 @@
 const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection');
-
+//create comment model
 class Comment extends Model {}
 
 Comment.init(
